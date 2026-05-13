9eedd14cdc7ef526ec3854588d7969c7ea90f670
Witara <rensofisbcn@gmail.com>
2026-05-13 22:48:42 +0200

    last commit of the day, added barriers

--- src/preload.js
+++ src/preload.js
@@ -2,6 +2,8 @@ export default class Preload extends Phaser.Scene {
   constructor() { super("Preload"); }
   preload() {
     this.load.image("chicken", "assets/chicken.png");
+    this.load.image("manhole", "assets/manhole_cover.png");
+    this.load.image("roadBarrier", "assets/roadBarrier.png");
   }
   create() { this.scene.start("Main"); }
 }
--- src/scenes/Main.js
+++ src/scenes/Main.js
@@ -3,6 +3,8 @@ export default class Main extends Phaser.Scene {
 
   create() {
     this.ch = this.add.image(60, 600, "chicken").setScale(-0.1, 0.1);
+    this.mh = this.add.image(400, 600, "manhole").setScale(0.1);
+    this.rb = this.add.image(400, 450, "roadBarrier").setScale(0.3);
   }
   update() {
 
