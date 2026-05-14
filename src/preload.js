1f7d92bd3a26e26c4762390b3dd8e0e9ffed2ab1
Ren-ofis <rensofisbcn@gmail.com>
2026-05-14 19:23:31 +0300

    added car asset

--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,7 @@
 export default class Preload extends Phaser.Scene {
   constructor() { super("Preload"); }
   preload() {
-    this.load.image("chicken", "assets/chicken.png");
-    this.load.image("manhole", "assets/manhole_cover.png");
-    this.load.image("roadBarrier", "assets/roadBarrier.png");
-    this.load.image("create", "assets/create.png");
+
 
   }
   create() { this.scene.start("Main"); }
